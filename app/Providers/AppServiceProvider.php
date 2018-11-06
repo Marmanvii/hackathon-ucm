bc59872a8bc9166b377b63f93b671a73a8227a9d
ClaudioDuran <claudiodurannunez13@gmail.com>
2018-11-06 01:06:29 -0300

    Comment deleted

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,11 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+
     public function boot()
     {
         //
