91b1f906e7d37c1f3eb4334e8832b435f3740a13
Marmanvii <marmanvi@gmail.com>
2018-11-08 06:14:59 -0300

    Modified boostrap files

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,12 +1,24 @@
-<form method="POST" style="text-align: center;width:80%; margin:0px auto;" action="/clients/result">
-  {{ csrf_field() }} <!--Sirve para evitar ataques modificando el html desde el navegador.-->
-  <div class="form-row">
-    <div class="col" align="center">
-      <div class="form-group col-md-8">
-        <label for="rut">RUT</label>
-        <input type="integer" class="form-control" id="rut" name="rut" required>
-      </div>
-    </div>
-      <button type="submit" align="center" class="btn">Seguir</button>
-    </div>
-  </form>
+@extends('layout.master')
+@section('content')
+<br><br><br><br>
+  <div class="card" style="text-align: center;width:50%; margin:0px auto;" align="center" >
+  <div class="card-header">
+    Buscar por RUT
+  </div>
+  <div class="card-body">
+    <form method="POST" style="text-align: center;width:80%; margin:0px auto;" action="/clients/result">
+      {{ csrf_field() }} <!--Sirve para evitar ataques modificando el html desde el navegador.-->
+      <div class="form-row">
+        <div class="col" align="center">
+          <div class="form-group col-md-8">
+            <label for="rut">RUT</label>
+            <input type="integer" class="form-control" id="rut" name="rut" required>
+          </div>
+        </div>
+          <button type="submit" align="center" class="btn btn-primary">Seguir</button>
+        </div>
+    </form>
+  </div>
+</div>
+<br><br><br><br>
+@endsection
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,15 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+    <!--<link rel="icon" href="../../favicon.ico">-->
 
     <title>Banco 3IT</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="css/carousel.css" rel="stylesheet">
   </head>
   <body>
     @include('layout.nav')
@@ -21,16 +17,8 @@
     @include('layout.footer')
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="js/vendor/holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
   </body>
 </html>
